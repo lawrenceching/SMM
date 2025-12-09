08a0e3ef29487a848ae6b8462345ac6dcb9e9f05
Lawrence Ching <lawrence.ching.zh@gmail.com>
2025-12-09 15:45:35 +0800

    implement statusbar

---
+++ core/types.ts
@@ -0,0 +1,14 @@
+/**
+ * Represent the application configuration, which not editable to the user.
+ */
+export interface AppConfig {
+    version: string;
+}
+
+
+/**
+ * Represent the user configuration, which is editable to the user.
+ */
+export interface UserConfig {
+
+}
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,19 @@
 import { Button } from "@/components/ui/button"
+import type { AppConfig } from "@core/types"
+
+const appConfig: AppConfig = {
+  version: "0.0.1"
+}
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
+      <div className="flex-1"></div>
       <Button>Click me</Button>
+      <div className="h-[30px] w-full bg-gray-400 flex">
+        <div className="flex-1"></div>
+        <div className="max-w-[60px]">{appConfig.version}</div>
+      </div>
     </div>
   )
 }
